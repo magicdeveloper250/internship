ec5264d3f327fea80655753f48f584625d84f24d
magicdeveloper250 <impanodeveloper@proton.me>
2026-04-23 10:37:28 +0200

    Changing the host names for both frontend and backend

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://internship00000.onrender.com",
     headers:{
         "Content-Type":"application/json"
     }
